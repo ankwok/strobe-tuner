f1d9ee230bdf2827dae8f00fb8f3220fc655ae75
Andrew Kwok <a.kwok@criteo.com>
2021-06-29 21:41:15 +0200

    Add Kalman filter UTests

---
+++ app/src/test/java/com/akwok/strobetuner/tuner/KalmanUpdaterUnitTest.kt
@@ -0,0 +1,47 @@
+package com.akwok.strobetuner.tuner
+
+import org.junit.Assert
+import org.junit.Test
+
+class KalmanUpdaterUnitTest {
+
+    @Test
+    fun estimateUnchangedForHugeMeasurementNoise() {
+        val initState = KalmanState(42.0, 1.0)
+        val updater = KalmanUpdater(initState, 1.0)
+        updater.update(1000.0, 1e20)
+
+        Assert.assertEquals(initState.x, updater.stateEstimate.x, 1e-4)
+    }
+
+    @Test
+    fun estimateIsMeasurementForSmallMeasurementNoise() {
+        val initState = KalmanState(42.0, 1.0)
+        val updater = KalmanUpdater(initState, 1.0)
+        updater.update(1000.0, 1e-20)
+
+        Assert.assertEquals(1000.0, updater.stateEstimate.x, 1e-4)
+    }
+
+    @Test
+    fun estimateUnchangedIfSameAsMeasurement() {
+        val initState = KalmanState(42.0, 1.0)
+        val updater = KalmanUpdater(initState, 1.0)
+        updater.update(initState.x, 1.0)
+
+        Assert.assertEquals(initState.x, updater.stateEstimate.x, 1e-4)
+    }
+
+    @Test
+    fun uncertaintyDecreasesOnRepeatedMeasurements() {
+        val initState = KalmanState(42.0, 1.0)
+        val updater = KalmanUpdater(initState, 1e-10)
+        updater.update(initState.x, 1.0)
+
+        val var1 = updater.stateEstimate.P
+        Assert.assertTrue(var1 < initState.P)
+
+        updater.update(initState.x, 1.0)
+        Assert.assertTrue(updater.stateEstimate.P < var1)
+    }
+}
