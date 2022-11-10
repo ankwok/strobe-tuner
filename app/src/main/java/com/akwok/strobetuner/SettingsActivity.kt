2f785426bd666cf33bec54bc73ae2bf3dc440e8e
Andrew Kwok <a.kwok@criteo.com>
2022-11-10 22:21:40 +0100

    Add Settings action bar

--- app/src/main/java/com/akwok/strobetuner/SettingsActivity.kt
+++ app/src/main/java/com/akwok/strobetuner/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.akwok.strobetuner
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,6 +11,22 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        supportActionBar?.title = getString(R.string.settings_title)
+
+        // Back button
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.orange_back_arrow)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
